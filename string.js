abfdbe1baf930168da6cb5fa6b1c6b38a3fc4a37
Extrance <38425275+Extrance@users.noreply.github.com>
2018-12-05 12:09:47 +0100

    Update string.js

--- string.js
+++ string.js
@@ -2,20 +2,22 @@ const express = require('express');
 const app = express();
 
 app.get('/square' , (req,res) => {
-  var s = req.query.string;
-  var a = {result: string_square(s)};
-  res.json(a)
-  res.end();
+  try{
+    var s = req.query.string;
+    var a = {result: string_square(s)};
+    res.json(a)
+    res.end();
+  }
+  catch(e) {
+    var a = {result: -1};
+    res.json(a)
+    res.end();
+  }
 });
 
 function string_square(s) {
-  try {
     var len = s.length;
     return len*len;
-  }
-  catch(e) {
-    return -1;
-  }
 }
 
 app.listen(4000 || process.env.PORT, () => console.log('App is online on port 4000'))
